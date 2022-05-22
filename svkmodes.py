ec7f6c493342bb19d3de217137998f1287f5d96d
unknown <imamfadhkur1@gmail.com>
2022-05-22 20:07:06 +0700

    menemukan cara untuk menyimpan data keyword dan mencari irisannya dengan intersect1d, data yang digunakan adalah list

--- svkmodes.py
+++ svkmodes.py
@@ -1,23 +1,76 @@
 # importing library
+from traceback import print_tb
 import xlrd
+import re
+import string
+import xlsxwriter
+import xlwt
 import sys
 # sys.exit()
 
 # initial cluster method
-def gicca(X,k):
-    centers = []
-    dens = []
-    for i in range(X.nrows):
+def gicca(X,k): # X = data buku beserta keyword, k = jumlah cluster
+    centers = [] # variabel yang digunakan untuk menyimpan center
+    dens = [] # variabel yang digunakan untuk menyimpan informasi density dari setiap buku
+    # awal code untuk mencari cluster center pertama
+    for i in range(X.nrows): # untuk data real akan di loop sebanyak 3585 kali
         dens_fitur = 0
-        for m in range((X.ncols)-1):
-            for n in range(X.nrows):
+        for m in range((X.ncols)-1): # loop 1 kali
+            for n in range(X.nrows): # loop 3585 kali
                 irisan_vajxi = 0
-                for vajxi in X.cell_value(rowx=i,colx=(m+1)).split(", "):
-                    if vajxi in X.cell_value(rowx=n,colx=(m+1)).split(", "):
+                # print(X.cell_value(rowx=i,colx=(m+1)),"|",X.cell_value(rowx=n,colx=(m+1)))
+                for vajxi in X.cell_value(rowx=i,colx=(m+1)).split(","): # melooping tiap keyword pada satu cell
+                    if vajxi in X.cell_value(rowx=n,colx=(m+1)).split(","):
                         irisan_vajxi += 1
-                dens_fitur += irisan_vajxi/(len(X.cell_value(rowx=i,colx=(m+1)).split(", "))+len(X.cell_value(rowx=n,colx=(m+1)).split(", "))-irisan_vajxi)
-        dens.append(dens_fitur/9)
+                dens_fitur += irisan_vajxi/(len(X.cell_value(rowx=i,colx=(m+1)).split(","))+len(X.cell_value(rowx=n,colx=(m+1)).split(","))-irisan_vajxi)
+        dens.append(dens_fitur/(X.nrows+1)) # setiap nilai dens_fitur dibagi banyaknya objek
     centers.append(dens.index(max(dens)))
+    # batas code batas mencari cluster center pertama
+    
+    # awal code mencari cluster center kedua
+    dm = []
+    for i in range(X.nrows): # loop sebanyak bukunya (3585)
+        dmd = 0
+        irisan_vasxi = 0
+        for m in range((X.ncols)-1): # 
+            for vasxi in X.cell_value(rowx=centers[0],colx=(m+1)).split(","): # untuk melooping tiap keyword dari center
+                if vasxi in X.cell_value(rowx=i,colx=(m+1)).split(","):
+                    irisan_vasxi += 1
+            dmd += 1-(irisan_vasxi/(len(X.cell_value(rowx=centers[0],colx=(m+1)).split(","))+len(X.cell_value(rowx=i,colx=(m+1)).split(","))-irisan_vasxi))
+        dm.append(dmd*dens[i])
+    centers.append(dm.index(max(dm)))
+    # batas code mencari cluster center kedua
+
+    # awal code mencari cluster center ketiga dst.
+    if k > 2:
+        for jumlah_cluster in range(k-2):            
+            minxm = len(centers)-1
+            dm = []
+            for i in range(X.nrows):
+                dmd = 0
+                irisan_vasxi = 0
+                for m in range((X.ncols)-1):
+                    for vasxi in X.cell_value(rowx=minxm,colx=(m+1)).split(","):
+                        if vasxi in X.cell_value(rowx=i,colx=(m+1)).split(","):
+                            irisan_vasxi += 1
+                    dmd += 1-(irisan_vasxi/(len(X.cell_value(rowx=minxm,colx=(m+1)).split(","))+len(X.cell_value(rowx=i,colx=(m+1)).split(","))-irisan_vasxi))
+                dm.append(dmd*dens[i])
+            print(dm.index(max(dm)))
+            
+            # if dm.index(max(dm)) in centers:
+            #     print("ada di centers")
+            #     nilai_terbesar = 0
+            #     for nilai_dm in range(len(dm)-1):
+            #         if dm[nilai_dm] > dm[nilai_dm+1]:
+            #             nilai_terbesar = dm[nilai_dm]
+            #         else:
+            #             nilai_terbesar = dm[nilai_dm+1]
+            # else:
+            #     centers.append(dm.index(max(dm)))
+            # exit()
+            # centers.append(dm.index(max(dm)))
+    # batas code mencari cluster center ketiga dst.
+
     return centers
 
 # update cluster center method
@@ -36,8 +89,8 @@ def run(item,k):
     initial_cluster_center = gicca(X,k)
     return initial_cluster_center
 
-data_file = "datatoybuku.xlsx"
+data_file = "write_data.xlsx" # bentuk data file excel yg berisi 2 kolom, kolom = A id buku, kolom B = daftar keyword yang dipisahkan dengan tanda koma. toy data ukuran 9*2
 
-jumlah_cluster = 3
+jumlah_cluster = 4
 data = run(data_file, jumlah_cluster)
 print(data)
--- testing.py
+++ testing.py
@@ -1,103 +1,57 @@
-# importing library
 import xlrd
-import re
-import string
-import xlsxwriter
-import xlwt
+import numpy as np
+# np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 import sys
 # sys.exit()
 
-# initial cluster method
-def gicca(X,k):
-    centers = []
-    dens = []
-    # awal code untuk mencari cluster center pertama
-    for i in range(X.nrows):
-        dens_fitur = 0
-        for m in range((X.ncols)-1):
-            for n in range(X.nrows):
-                irisan_vajxi = 0
-                for vajxi in X.cell_value(rowx=i,colx=(m+1)).split(", "):
-                    if vajxi in X.cell_value(rowx=n,colx=(m+1)).split(", "):
-                        irisan_vajxi += 1
-                dens_fitur += irisan_vajxi/(len(X.cell_value(rowx=i,colx=(m+1)).split(", "))+len(X.cell_value(rowx=n,colx=(m+1)).split(", "))-irisan_vajxi)
-        dens.append(dens_fitur/9)
-    centers.append(dens.index(max(dens)))
-    # batas code batas mencari cluster center pertama
-    
-    # awal code mencari cluster center kedua
-    dm = []
-    for i in range(X.nrows):
-        dmd = 0
-        irisan_vasxi = 0
-        for m in range((X.ncols)-1):
-            for vasxi in X.cell_value(rowx=centers[0],colx=(m+1)).split(", "):
-                if vasxi in X.cell_value(rowx=i,colx=(m+1)).split(", "):
-                    irisan_vasxi += 1
-            dmd += 1-(irisan_vasxi/(len(X.cell_value(rowx=centers[0],colx=(m+1)).split(", "))+len(X.cell_value(rowx=i,colx=(m+1)).split(", "))-irisan_vasxi))
-        dm.append(dmd*dens[i])
-    centers.append(dm.index(max(dm)))
-    # batas code mencari cluster center kedua
-
-    return dens
-
-# update cluster center method
-def hafsm():
-    return True
-
-# clustering method
-def svkmodes(X,k):
-    return True
-
-# preprocessing method
-def preprocessing(X):
-    databefore = []
-    dataafter = []
-    tanda_baca = ""
-    for ww in string.punctuation:
-        if ww == "," or ww == ";":
+item = xlrd.open_workbook("write_data.xlsx")
+X = item.sheet_by_index(0)
+data = np.array([[]])
+nparray = np.array([])
+for m in range(X.ncols-1):
+      for n in range(X.nrows):
             pass
-        else:
-            tanda_baca += ww
-    # print(tanda_baca)
-    workbook = xlsxwriter.Workbook('write_data.xlsx')
-    worksheet = workbook.add_worksheet()
-    for m in range((X.ncols)):
-        for n in range(X.nrows):
-            # print(X.cell_value(rowx=n,colx=(m)),"|",X.cell_value(rowx=n,colx=(m+1)).lower())
-            # databefore.append(X.cell_value(rowx=n,colx=(m+1)))
-            # dataafter.append(X.cell_value(rowx=n,colx=(m+1)).lower())
-            if m > 0:
-                dataafter_satuan = X.cell_value(rowx=n,colx=(m)).lower()
-                # print(dataafter_satuan)
-                # print("===================")
-                dataafter_satuan = re.sub(r"\d+", "", dataafter_satuan)
-                dataafter_satuan = dataafter_satuan.translate(str.maketrans(";",",",tanda_baca))
-                dataafter_satuan = re.sub('\s+','',dataafter_satuan)
-            else:
-                dataafter_satuan = X.cell_value(rowx=n,colx=(m))
-            
-            worksheet.write(n,m,dataafter_satuan)
-
-            # print(dataafter_satuan)
-            # print("")
-    workbook.close()
-    # print(databefore)
-    # dataafter = re.sub(r"\d+", "", dataafter)
-    # print(dataafter)
-    
-
-# main method
-def run(item,k):
-    item = xlrd.open_workbook(item)
-    X = item.sheet_by_index(0)
-    preprocessing(X)
-    # menentukan insial cluster center
-    # initial_cluster_center = gicca(X,k)
-    # return initial_cluster_center
-
-data_file = "datatoybuku.xlsx"
-
-jumlah_cluster = 3
-data = run(data_file, jumlah_cluster)
+            # print(X.cell_value(rowx=n,colx=(m+1)).split(","))
+            # data = np.append(data,X.cell_value(rowx=n,colx=(m+1)).split(","),axis=0)
+      # data.append(nparray)
 # print(data)
+# print("=================")
+# print(nparray[0][0])
+# exit()
+# step_n = 10
+# fitur = []
+# steps = np.array([])
+# print(steps)
+fiturs = []
+for m in range(X.ncols-1):
+      fitur = []
+      for n in range(X.nrows):
+            # step = np.array(X.cell_value(rowx=n,colx=(m+1)).split(","),dtype=object)
+            fitur.append(X.cell_value(rowx=n,colx=(m+1)).split(","))
+            # print(step,step1)
+            # print(len(np.intersect1d(step, step1)))
+            # steps = np.append(steps, step, axis=0)
+            # print(steps)
+            # exit()
+            # data = np.append(data,[X.cell_value(rowx=n,colx=(m+1))],axis=0)
+      fiturs.append(fitur)
+print(fiturs[0][1],fiturs[0][2])
+print(len(np.intersect1d(fiturs[0][1], fiturs[0][2]))) # intersect1d (untuk mencari IRISAN DARI DUA LIST)
+# for n in range(step_n-1):
+#     step = np.array([[3,4]])
+#     steps = np.append(steps, step, axis=0)
+# steps = np.delete(steps,1,0)
+# print(steps)
+
+arr1 = np.array([
+  ["ekonomi","moneter"],
+  ["ekonomi","mikro"],
+  ["ekonomi"],
+  ["coreldraw"],
+  ["kepemimpinan","manusia"]],dtype=object)
+arr2 = np.array(["ekonomi","mikro"],dtype=object)
+newarr = np.intersect1d(arr2, arr1[1])
+# data = [arr1,arr2]
+# print(arr1)
+# print(len(newarr))
+# print(arr1)
